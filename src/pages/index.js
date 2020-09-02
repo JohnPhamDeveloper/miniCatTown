42c9554bb4dd949577f35bcdec69dc8fec71c38c
JohnPhamDeveloper <JohnPhamDeveloper@hotmail.com>
2020-09-02 16:48:57 -0700

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,63 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-import get from 'lodash/get'
-import { Helmet } from 'react-helmet'
-import Hero from '../components/hero'
-import Layout from '../components/layout'
-import ArticlePreview from '../components/article-preview'
+import '../scss/reset.scss'
+import '../scss/typography.scss'
+import './index.scss'
+// import get from 'lodash/get'
+// import { Helmet } from 'react-helmet'
+// import Hero from '../components/hero'
+// import Layout from '../components/layout'
+// import ArticlePreview from '../components/article-preview'
 
-class RootIndex extends React.Component {
-  render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allContentfulBlogPost.edges')
-    const [author] = get(this, 'props.data.allContentfulPerson.edges')
+const RootIndex = ({ data }) => {
+  const siteTitle = data.site.siteMetadata.title
+  const posts = data.allContentfulBlogPost.edges
+  const [author] = data.allContentfulPerson.edges
 
-    return (
-      <Layout location={this.props.location}>
-        <div style={{ background: '#fff' }}>
-          <Helmet title={siteTitle} />
-          <Hero data={author.node} />
-          <div className="wrapper">
-            <h2 className="section-headline">Recent articles</h2>
-            <ul className="article-list">
-              {posts.map(({ node }) => {
-                return (
-                  <li key={node.slug}>
-                    <ArticlePreview article={node} />
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
-        </div>
-      </Layout>
-    )
-  }
+  return (
+    <div>
+      <h1>{siteTitle}</h1>
+      {posts.map(({ node }) => (
+        <li key={node.slug}>
+          <h2>{node.slug}</h2>
+          <p>I'm a paragraph</p>
+        </li>
+      ))}
+    </div>
+  )
 }
+
+// class RootIndex extends React.Component {
+//   render() {
+//     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
+//     const posts = get(this, 'props.data.allContentfulBlogPost.edges')
+//     const [author] = get(this, 'props.data.allContentfulPerson.edges')
+
+//     return <div>clean</div>
+
+//     // return (
+//     //   <Layout location={this.props.location}>
+//     //     <div style={{ background: '#fff' }}>
+//     //       <Helmet title={siteTitle} />
+//     //       <Hero data={author.node} />
+//     //       <div className="wrapper">
+//     //         <h2 className="section-headline">Recent articles</h2>
+//     //         <ul className="article-list">
+//     //           {posts.map(({ node }) => {
+//     //             return (
+//     //               <li key={node.slug}>
+//     //                 <h2>{node.slug}</h2>
+//     //                 <ArticlePreview article={node} />
+//     //               </li>
+//     //             )
+//     //           })}
+//     //         </ul>
+//     //       </div>
+//     //     </div>
+//     //   </Layout>
+//     // )
+//   }
+// }
 
 export default RootIndex
 
