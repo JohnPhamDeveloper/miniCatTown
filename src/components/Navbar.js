da881a9561d4cfc2edd9297e6ed1bb7d5fc9871b
JohnPhamDeveloper <JohnPhamDeveloper@hotmail.com>
2020-09-03 02:35:02 -0700

    Add font

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,6 +10,11 @@ const Navbar = () => {
       </Link>
       {/* Mini cat town text */}
       {/* Navlinks */}
+      <button class="hamburger hamburger--collapse is-active" type="button">
+        <span class="hamburger-box">
+          <span class="hamburger-inner"></span>
+        </span>
+      </button>
     </nav>
   )
 }
