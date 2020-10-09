21f5e1878afbf81441a7f146ac6275c23fb09cd6
JohnPhamDeveloper <JohnPhamDeveloper@hotmail.com>
2020-10-09 02:21:56 -0700

    How you can help section done

--- src/components/CTAMultipleLayout.js
+++ src/components/CTAMultipleLayout.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const CTAMultipleLayout = ({ children }) => {
-  return <div className="cta-multiple-layout">{children}</div>
+  return (
+    <div className="cta-multiple-layout">
+      <div className="border-top"></div>
+      <h1 className="cta-multiple-layout__title">How You Can Help</h1>
+      <div className="cta-multiple-layout__wrapper">{children}</div>
+      <div className="border-bottom"></div>
+    </div>
+  )
 }
 
 export default CTAMultipleLayout
